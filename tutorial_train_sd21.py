9eb0dcc49b31946d4a8c5669ad614b0f2e3ec0aa
RoscaMitrut <reduardmitrut@gmail.com>
2024-11-27 21:34:35 +0000

    removed best (not working)

--- tutorial_train_sd21.py
+++ tutorial_train_sd21.py
@@ -11,7 +11,7 @@ from pytorch_lightning.loggers import TensorBoardLogger
 # Configs
 resume_path = './models/control_sd21_ini.ckpt'
 batch_size = 16
-logger_freq = 146
+logger_freq = 140
 learning_rate = 1e-5
 sd_locked = True
 only_mid_control = False
@@ -31,15 +31,6 @@ dataloader = DataLoader(dataset, num_workers=12, batch_size=batch_size, shuffle=
 logger = ImageLogger(batch_frequency=logger_freq)
 logger2 = TensorBoardLogger("tb_logs", name="my_model")
 
-best_model_checkpoint = ModelCheckpoint(
-    dirpath='./checkpoints/',
-    filename='best-model-{epoch:02d}-{val_loss:.2f}',  # Include the metric in the filename
-    monitor='train/loss_epoch',  # Replace with the metric you want to monitor
-    mode='min',  # 'min' for loss, 'max' for accuracy, etc.
-    save_top_k=1,  # Save only the best model
-    save_weights_only=True
-)
-
 # Save the last model
 last_model_checkpoint = ModelCheckpoint(
     dirpath='./checkpoints/',
@@ -49,7 +40,7 @@ last_model_checkpoint = ModelCheckpoint(
     save_weights_only=True
 )
 
-trainer = pl.Trainer(gpus=1, precision=32, logger=logger2, callbacks=[logger,best_model_checkpoint,last_model_checkpoint], max_epochs=10)
+trainer = pl.Trainer(gpus=1, precision=32, logger=logger2, callbacks=[logger,last_model_checkpoint], max_epochs=10)
 
 # Train!
 trainer.fit(model, dataloader)
