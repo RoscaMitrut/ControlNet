bd98ac8293efbfa5c9da5c28cc059f327bbd0750
RoscaMitrut <reduardmitrut@gmail.com>
2024-11-27 21:10:57 +0000

    edited dataset load and training

--- tutorial_train_sd21.py
+++ tutorial_train_sd21.py
@@ -2,15 +2,16 @@ from share import *
 
 import pytorch_lightning as pl
 from torch.utils.data import DataLoader
+from pytorch_lightning.callbacks import ModelCheckpoint
 from tutorial_dataset import MyDataset
 from cldm.logger import ImageLogger
 from cldm.model import create_model, load_state_dict
-
+from pytorch_lightning.loggers import TensorBoardLogger
 
 # Configs
 resume_path = './models/control_sd21_ini.ckpt'
-batch_size = 4
-logger_freq = 300
+batch_size = 16
+logger_freq = 146
 learning_rate = 1e-5
 sd_locked = True
 only_mid_control = False
@@ -26,10 +27,29 @@ model.only_mid_control = only_mid_control
 
 # Misc
 dataset = MyDataset()
-dataloader = DataLoader(dataset, num_workers=0, batch_size=batch_size, shuffle=True)
+dataloader = DataLoader(dataset, num_workers=12, batch_size=batch_size, shuffle=True)
 logger = ImageLogger(batch_frequency=logger_freq)
-trainer = pl.Trainer(gpus=1, precision=32, callbacks=[logger])
+logger2 = TensorBoardLogger("tb_logs", name="my_model")
 
+best_model_checkpoint = ModelCheckpoint(
+    dirpath='./checkpoints/',
+    filename='best-model-{epoch:02d}-{val_loss:.2f}',  # Include the metric in the filename
+    monitor='train/loss_epoch',  # Replace with the metric you want to monitor
+    mode='min',  # 'min' for loss, 'max' for accuracy, etc.
+    save_top_k=1,  # Save only the best model
+    save_weights_only=True
+)
+
+# Save the last model
+last_model_checkpoint = ModelCheckpoint(
+    dirpath='./checkpoints/',
+    filename='last-model',
+    save_top_k=1,  # Always keep only the last model
+    save_last=True,  # Ensure the last model is saved
+    save_weights_only=True
+)
+
+trainer = pl.Trainer(gpus=1, precision=32, logger=logger2, callbacks=[logger,best_model_checkpoint,last_model_checkpoint], max_epochs=10)
 
 # Train!
 trainer.fit(model, dataloader)
